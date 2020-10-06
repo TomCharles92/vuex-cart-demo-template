c8db16b4c84a6297ab11753631ceae20ac41613c
TomCharles92 <zhatao1992@163.com>
2020-10-06 17:34:42 +0800

    购物车组件完成

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,7 +1,20 @@
 const state = {
   cartProducts: []
 }
-const getters = {}
+const getters = {
+  totalCount (state) {
+    return state.cartProducts.reduce((sum, prod) => {
+      if (prod.isChecked) sum += prod.count
+      return sum
+    }, 0)
+  },
+  totalPrice (state) {
+    return state.cartProducts.reduce((sum, prod) => {
+      if (prod.isChecked) sum += prod.count * prod.price
+      return sum
+    }, 0)
+  }
+}
 const mutations = {
   addToCart (state, product) {
     const prod = state.cartProducts.find(item => item.id === product.id)
@@ -17,6 +30,26 @@ const mutations = {
         totalPrice: product.price
       })
     }
+  },
+  deleteProd (state, prodId) {
+    const index = state.cartProducts.findIndex(prod => prod.id === prodId)
+    if (index !== -1) {
+      state.cartProducts.splice(index, 1)
+    }
+  },
+  // 全部“选中”
+  updateAllProductChecked (state, value) {
+    state.cartProducts.forEach(prod => {
+      prod.isChecked = value
+    })
+  },
+  updateProductChecked (state, { id, isChecked }) {
+    const prod = state.cartProducts.find(prod => prod.id === id)
+    prod && (prod.isChecked = isChecked)
+  },
+  addProductCount (state, { id, count }) {
+    const prod = state.cartProducts.find(prod => prod.id === id)
+    prod && (prod.count = count)
   }
 }
 const actions = {}
--- src/views/cart.vue
+++ src/views/cart.vue
@@ -11,8 +11,7 @@
       <el-table-column
         width="55">
         <template v-slot:header>
-          <el-checkbox size="mini">
-          </el-checkbox>
+          <el-checkbox v-model="checkAll" size="mini"></el-checkbox>
         </template>
         <!--
           @change="updateProductChecked"  默认参数：更新后的值
@@ -23,6 +22,7 @@
           <el-checkbox
             size="mini"
             :value="scope.row.isChecked"
+            @change="updateProductChecked({ id: scope.row.id, isChecked: $event})"
           >
           </el-checkbox>
         </template>
@@ -38,8 +38,11 @@
       <el-table-column
         prop="count"
         label="数量">
-        <template>
-          <el-input-number size="mini"></el-input-number>
+        <template v-slot="scope">
+          <el-input-number :value="scope.row.count" @change="addProductCount({
+            id: scope.row.id,
+            count: $event
+          })" size="mini"></el-input-number>
         </template>
       </el-table-column>
       <el-table-column
@@ -48,30 +51,40 @@
       </el-table-column>
       <el-table-column
         label="操作">
-        <template>
-          <el-button size="mini">删除</el-button>
+        <template v-slot="scope">
+          <el-button size="mini" @click="deleteProd(scope.row.id)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
     <div>
-      <p>已选 <span>xxx</span> 件商品，总价：<span>xxx</span></p>
+      <p>已选 <span>{{ totalCount }}</span> 件商品，总价：<span>{{ totalPrice }}</span></p>
       <el-button type="danger">结算</el-button>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapMutations, mapState } from 'vuex'
 
 export default {
   name: 'Cart',
   data () {
-    return {
-      cartProducts: [
-        { id: 1, title: 'iPad Pro', price: 500.01 },
-        { id: 2, title: 'H&M T-Shirt White', price: 10.99 },
-        { id: 3, title: 'Charli XCX - Sucker CD', price: 19.99 }
-      ]
+    return {}
+  },
+  computed: {
+    ...mapState('cart', ['cartProducts']),
+    ...mapGetters('cart', ['totalCount', 'totalPrice']),
+    checkAll: {
+      get () {
+        return this.cartProducts.every(prod => prod.isChecked)
+      },
+      set (value) {
+        this.updateAllProductChecked(value)
+      }
     }
+  },
+  methods: {
+    ...mapMutations('cart', ['deleteProd', 'updateAllProductChecked', 'updateProductChecked', 'addProductCount'])
   }
 }
 </script>
